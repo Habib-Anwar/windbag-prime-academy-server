1dd46731744c0a32e4a9216c77c3cb2760a5d39a
Habib-Anwar <121849760+Habib-Anwar@users.noreply.github.com>
2023-06-11 05:22:22 +0600

    delete course

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.87bzbwh.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -51,6 +51,13 @@ async function run() {
       const item = req.body;
       console.log(item);
       const result = await courseCollection.insertOne(item);
+      res.send(result);
+    })
+
+    app.delete('/courses/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id) };
+      const result = await courseCollection.deleteOne(query);
       res.send(result);
     })
 
