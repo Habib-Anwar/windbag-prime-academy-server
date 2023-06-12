ba57efecb8f4334a13971c5d6e21a08c01b51709
Habib-Anwar <121849760+Habib-Anwar@users.noreply.github.com>
2023-06-12 23:43:12 +0600

    final submission

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   client.connect();
 
     const usersCollection = client.db("windbagDb").collection("users");
     const classCollection = client.db("windbagDb").collection("classes");
